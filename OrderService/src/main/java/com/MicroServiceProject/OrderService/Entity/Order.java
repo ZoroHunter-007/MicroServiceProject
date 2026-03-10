cf0fed0392c70db4addd7a56495c8ed260b6e36f
Dhruv <dhruvmalusare13@gmail.com>
2026-03-10 22:42:20 +0530

    Added all files

--- OrderService/src/main/java/com/MicroServiceProject/OrderService/Entity/Order.java
+++ OrderService/src/main/java/com/MicroServiceProject/OrderService/Entity/Order.java
@@ -11,11 +11,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "orders")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
